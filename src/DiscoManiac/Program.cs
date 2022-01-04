2613f41f95768d716df3b401cf844b6beb958b35
Kristofer Liljeblad <kristofer.liljeblad@live.com>
2022-01-04 18:08:29 +0100

    Quick and dirty high-score implementation

--- src/DiscoManiac/Program.cs
+++ src/DiscoManiac/Program.cs
@@ -1,8 +1,14 @@
-﻿using System.ComponentModel.Design;
+﻿using System.Runtime.Serialization.Json;
+using System.Security.AccessControl;
+using System.Text;
+using System.ComponentModel.Design;
 using Discord;
 using Discord.WebSocket;
+using System.Text.Json;
 
 Console.WriteLine("Disco Maniac Starting!");
+
+var highScores = new Dictionary<string, int>();
 
 var client = new DiscordSocketClient();
 client.Log += LogAsync;
@@ -19,19 +25,81 @@ await Task.Delay(Timeout.Infinite);
 
 
 
+(bool isCommand, string command, string[] args) GetCommand(char commandPrefix, string message)
+{
+    if (string.IsNullOrWhiteSpace(message)) 
+    {
+        return (false, "", null);
+    }
 
+    if (message.Length < 2 || message[0] != commandPrefix)
+    {
+        return (false, "", null);
+    }
 
-
+    var messageWithoutPrefix = message.Substring(1);
+    var messageAsWords = messageWithoutPrefix.Split();
+    var command = messageAsWords[0].ToLower();
+    var args = messageAsWords[1..];
+    
+    return (true, command, args);
+}
 
 async Task MessageReceivedAsync(SocketMessage message)
 {
     if (message.Author.Id == client.CurrentUser.Id)
         return;
 
-    Console.WriteLine(message.Content);
+    string author = message.Author.ToString();
 
-    if (message.Content == "!ping")
-        await message.Channel.SendMessageAsync("pong!");
+    Console.WriteLine($"DEBUG - Message: {message.Content}, From: {author}");
+    
+    var newScore = message.Content.Length;
+
+    // Check if user exists
+    if (highScores.ContainsKey(author))
+    {
+        var currentScore = highScores[author];
+        highScores[author] = currentScore + newScore;
+    }
+    else
+    {
+        highScores.Add(author, newScore);
+    }
+
+
+    var (isCommand, command, args) = GetCommand('!', message.Content);
+
+    if (!isCommand)
+        return;
+
+    if (command == "show-scores")
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendLine("High Score");
+
+        foreach (var item in highScores)
+        {
+            sb.AppendLine($"{item.Key} - {item.Value}");
+        }
+
+        message.Channel.SendMessageAsync(sb.ToString());
+    }
+    else if (command == "save-scores")
+    {
+        Console.WriteLine("Saving scores");
+        using var file = File.Create("saved-scores.json");
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        await JsonSerializer.SerializeAsync(file, highScores, options);
+    }
+    else if (command == "load-scores")
+    {
+        Console.WriteLine("Loading scores...");
+        var json = File.ReadAllText("saved-scores.json");
+        highScores = JsonSerializer.Deserialize<Dictionary<string, int>>(json);
+        Console.WriteLine("New scores loaded");
+    }
 }
 
 Task LogAsync(LogMessage log)
