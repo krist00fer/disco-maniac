356c35c5146cebb1f6de6ff9153987bc0af4e182
Kristofer Liljeblad <kristofer.liljeblad@live.com>
2022-01-04 15:50:52 +0100

    First working ping bot implemented

--- src/DiscoManiac/Program.cs
+++ src/DiscoManiac/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+using Discord;
+using Discord.WebSocket;
+
+Console.WriteLine("Disco Maniac Starting!");
+
+var client = new DiscordSocketClient();
+client.Log += LogAsync;
+client.Ready += ReadyAsync;
+client.MessageReceived += MessageReceivedAsync;
+
+var token = Environment.GetEnvironmentVariable("DISCO_BOT_TOKEN");
+
+Console.WriteLine($"Token: {token}");
+
+await client.LoginAsync(TokenType.Bot, token);
+await client.StartAsync(); 
+await Task.Delay(Timeout.Infinite);
+
+
+
+
+
+
+
+async Task MessageReceivedAsync(SocketMessage message)
+{
+    if (message.Author.Id == client.CurrentUser.Id)
+        return;
+
+    Console.WriteLine(message.Content);
+
+    if (message.Content == "!ping")
+        await message.Channel.SendMessageAsync("pong!");
+}
+
+Task LogAsync(LogMessage log)
+{
+    Console.WriteLine("Nu loggar jag");
+    Console.WriteLine(log.ToString());
+    return Task.CompletedTask;
+}
+
+Task ReadyAsync()
+{
+    Console.WriteLine($"{client.CurrentUser} is connected!");
+
+    return Task.CompletedTask;
+}
